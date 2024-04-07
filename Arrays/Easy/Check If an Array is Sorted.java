7154ffa1cae15da063a1e4e657b4221fec98545b
Faisal khan <119971851+Faisal-khann@users.noreply.github.com>
2024-04-08 04:30:13 +0530

    Update Check If an Array is Sorted.java

--- Arrays/Easy/Check If an Array is Sorted.java
+++ Arrays/Easy/Check If an Array is Sorted.java
@@ -1,5 +1,5 @@
 /*
- * Check if an Array is Sorted
+  Check if an Array is Sorted
    Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
    
    Note: Two consecutive equal values are considered to be sorted.
